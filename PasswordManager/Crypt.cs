71f4a93af6cbbb30b183167cdb5eaea3d05aec7a
Mateusz Pieczulis <mateuszpieczulis123@gmail.com>
2024-03-05 09:53:47 +0100

    changed encryption algorithm

--- PasswordManager/Crypt.cs
+++ PasswordManager/Crypt.cs
@@ -1,45 +1,64 @@
-﻿namespace PasswordManager
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordManager
 {
     public class Crypt
     {
-        public string encrypt(string input)
+        public string key = "b14ca5898a4e4133bbce2ea2315a1916";
+        public string encrypt(string plainText)
         {
-            string output = "";
-            foreach (char c in input)
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
             {
-                if (char.IsLetter(c))
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
                 {
-                    char cc = c;
-                    if (c == 'z') cc = 'a';
-                    if (c == 'Z') cc = 'A';
-                    output += (char)(((int)cc) + 5);
-                }
-                else
-                {
-                    output += c;
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
                 }
             }
-            return output;
+
+            return Convert.ToBase64String(array);
         }
 
-        public string decrypt(string input)
+        public string decrypt(string cipherText)
         {
-            string output = "";
-            foreach (char c in input)
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
             {
-                if (char.IsLetter(c))
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
                 {
-                    char cc = c;
-                    if (c == 'z') cc = 'a';
-                    if (c == 'Z') cc = 'A';
-                    output += (char)(((int)cc) - 5);
-                }
-                else
-                {
-                    output += c;
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
                 }
             }
-            return output;
         }
     }
 }
